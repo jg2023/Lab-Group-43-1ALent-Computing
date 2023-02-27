1c17664a22f4e5987327a282c1ab42c82de05abc
Ben-Smith-03 <bs731@cam.ac.uk>
2023-02-27 21:19:08 +0000

    Task 2E Fixed FINALLY

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,6 +3,7 @@ import matplotlib
 
 def plot_water_levels(station,dates,levels):
     """This procedure plots water level against time for a certain station"""
+    plt.rcParams['figure.figsize']=[18,10]
     plt.plot(dates,levels)
     plt.axhline(station.typical_range[0],color = 'g',label = 'Low typical range boundary')
     plt.axhline(station.typical_range[1],color = 'r',label = 'High typical range boundary')  
